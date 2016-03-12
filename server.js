d634336a1eed5a2caa7de4d33897981f50857e75
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2016-03-12 09:28:13 +0900

    add node server.js for test.

---
+++ server.js
@@ -0,0 +1,18 @@
+var server = require('http').createServer();
+var fs = require('fs');
+
+server.on('request', function(req, res){
+  fs.readFile(__dirname + (req.url === '/' ? '/demo.html' : req.url), 'utf-8', function(err, data){
+    if(err){
+      res.writeHead(404, {"Content-Type": 'text/plain'});
+      res.write('not found.');
+    }else{
+      res.writeHead(200, {"Content-Type": 'text/html'});
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(3333, '127.0.0.1');
+console.log('listening http://localhost:3333/');
